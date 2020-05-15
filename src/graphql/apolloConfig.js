0dc70ab06d3be9cfa2e9f2d60252e5e407978041
Oscar Carnero <Oscar.Carnero@endava.com>
2020-05-15 13:10:11 -0300

    graphql error handling

--- src/graphql/apolloConfig.js
+++ src/graphql/apolloConfig.js
@@ -4,8 +4,10 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' })
+const graphLink = new HttpLink({ uri: 'http://localhost:3000/graphql' })
 
 const httpLinkAuth = setContext((_, { headers }) => {
   // get the authentication token from localstorage if it exists
@@ -20,9 +22,31 @@ const httpLinkAuth = setContext((_, { headers }) => {
   }
 })
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  let msgs = null
+
+  if (networkError) {
+    msgs = `[Network error]: ${networkError}`
+  }
+
+  if (graphQLErrors) {
+    msgs += msgs ? ' - ' : ''
+    msgs += '[GraphQL error]: '
+    let count = 0
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      // msgs += `Message ${count}: ${message}, Location: ${locations}, Path: ${path}`
+      msgs += `Message ${count}: ${message}; `
+      count++
+    })
+  }
+  console.log(msgs)
+})
+
+const link = ApolloLink.from([httpLinkAuth, errorLink, graphLink])
+
 // Create the apollo client
 export const apolloClient = new ApolloClient({
-  link: httpLinkAuth.concat(httpLink),
+  link: link,
   cache: new InMemoryCache()
 })
 
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -10,11 +10,13 @@ export const LOGIN_MUTATION = gql`
   }
 `
 export const NEW_USER_MUTATION = gql`
-  mutation addUser($fullName: String!, $email: String!, $role: String!) {
-    addUser(fullName: $fullName, email: $email, role: $role) {
-      success,
-      message,
-      user
+  mutation addUser {
+    addUser (addUserReq: {
+      fullName: $fullName,
+      email: $email,
+      role: $role
+    }) {
+      id
     }
   }
 `
